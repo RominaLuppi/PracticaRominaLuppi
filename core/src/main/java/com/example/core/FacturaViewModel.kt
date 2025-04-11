fc7432cbb0adb8f6fa96394dc4c18336be0e95cd
Romina Luppi <rominaluppi@gmail.com>
2025-04-11 15:14:56 +0200

    Commit 4: Pantalla de filtros finalizada. Falta acción del btn Aplicar

--- core/src/main/java/com/example/core/FacturaViewModel.kt
+++ core/src/main/java/com/example/core/FacturaViewModel.kt
@@ -1,12 +1,58 @@
 package com.example.core
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.domain.Factura
+import java.util.EnumSet.of
 
 class FacturaViewModel: ViewModel() {
 
     private val _facturas = MutableLiveData<List<Factura>>(emptyList())
     val facturas: LiveData<List<Factura>> get() = _facturas
+
+
+    var fechaDesde by mutableStateOf<Long?>(null)
+        private set
+    var fechaHasta by mutableStateOf<Long?>(null)
+        private set
+
+    var sliderPosition by mutableStateOf(0f)
+        private set
+
+    var checkedState by mutableStateOf(List(5){false})
+        private set
+
+    fun ActualizarFechaDesde(date: Long?){
+        fechaDesde = date
+    }
+    fun ActualizarFechaHasta(date: Long?){
+        fechaHasta = date
+    }
+
+    fun SelectorImporte(value: Float){
+        sliderPosition = value
+    }
+
+    fun SelectorEstado(index: Int, value: Boolean){
+        checkedState = checkedState.toMutableList().apply { this[index] = value }
+
+    }
+    fun ResetearFechas(){
+        fechaDesde = null
+        fechaHasta = null
+    }
+
+    fun ResetearSlider(){
+        sliderPosition = 0f
+    }
+
+    fun ResetearCheckBox(){
+        checkedState = List(5) {false}
+
+    }
+
 }
--- core/src/main/java/com/example/core/FiltroScreen.kt
+++ core/src/main/java/com/example/core/FiltroScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Divider
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -23,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import java.text.SimpleDateFormat
 import java.util.*
@@ -77,34 +80,32 @@ fun FiltrosScreen(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Start
             )
             {
-                Column {
+                Column(modifier = Modifier.padding(end = 20.dp)) {
                     Text(
                         text = stringResource(R.string.fecha_desde),
-                        modifier = Modifier.padding(16.dp),
+                        modifier = Modifier.padding(bottom = 8.dp),
                         color = Color.Gray,
-                        fontSize = 18.sp
+                        fontSize = 16.sp
                     )
-                    CalendarioFechas()
+                    CalendFechaDesde()
                 }
-                Spacer(modifier = Modifier.width(16.dp))
 
-                Column {
+                Column() {
                     Text(
                         text = stringResource(R.string.fecha_hasta),
-                        modifier = Modifier.padding(16.dp),
+                        modifier = Modifier.padding(bottom = 8.dp),
                         color = Color.Gray,
-                        fontSize = 18.sp
+                        fontSize = 16.sp
                     )
-                    CalendarioFechas()
+                    CalendFechaHasta()
                 }
-
             }
             Divider(
-                modifier = Modifier.padding(start = 16.dp, top = 30.dp, end = 16.dp),
+                modifier = Modifier.padding(start = 16.dp, top = 20.dp, end = 16.dp),
                 color = Color.LightGray,
                 thickness = 1.dp,
                 startIndent = 0.dp
@@ -113,16 +114,14 @@ fun FiltrosScreen(
             Spacer(modifier = Modifier.width(16.dp))
 
             Text(
-                text = stringResource(R.string.text_filtos),
-                modifier = Modifier.padding(top = 30.dp, start = 16.dp, bottom = 24.dp),
+                text = stringResource(R.string.text_por_importe),
+                modifier = Modifier.padding(top = 26.dp, start = 16.dp, bottom = 6.dp),
                 color = Color.Black,
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold
             )
 
-            Slider(
-                importeMax = 25.90f
-            )
+            SeleccionarImporte(importeMax = 300.0f)
 
             Divider(
                 modifier = Modifier.padding(start = 16.dp, top = 30.dp, end = 16.dp),
@@ -130,48 +129,168 @@ fun FiltrosScreen(
                 thickness = 1.dp,
                 startIndent = 0.dp
             )
+            SeleccionarEstado()
+
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AplicarFiltros()
+                EliminarFiltros()
+            }
 
         }
+            }
 
+}
 
-    }
-    Row {
-
+@Composable
+fun AplicarFiltros() {
+    Button(
+        modifier = Modifier.width(250.dp)
+            .padding(bottom = 16.dp),
+        onClick = {
+            TODO()
+        },
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.screen_fact_color))
+    ) {
+        Text(
+            text = stringResource(R.string.btn_aplicar),
+            color = Color.White
+        )
     }
 }
 
 @Composable
-fun CalendarioFechas(modifier: Modifier = Modifier) {
-    var selectedDate by remember { mutableStateOf<Long?>(null) }
+fun EliminarFiltros(viewModel: FacturaViewModel = viewModel()) {
+    Button(
+        modifier = Modifier.width(250.dp),
+        onClick = {
+            viewModel.ResetearFechas()
+            viewModel.ResetearSlider()
+            viewModel.ResetearCheckBox()
+        },
+        colors = ButtonDefaults.buttonColors(Color.Gray)
+    ) {
+        Text(
+            text = stringResource(R.string.btn_eliminar_filtros),
+            color = Color.White
+        )
+    }
+
+}
+
+@Composable
+fun SeleccionarEstado(viewModel: FacturaViewModel = viewModel()) {
+    val checkedState = viewModel.checkedState
+    val label = listOf(
+        stringResource(R.string.pagadas),
+        stringResource(R.string.anuladas),
+        stringResource(R.string.cuota_fija),
+        stringResource(R.string.pendientes),
+        stringResource(R.string.plan_pago)
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp, end = 16.dp, start = 16.dp)
+    ) {
+
+        Text(
+            text = stringResource(R.string.text_por_estado),
+            modifier = Modifier.padding(top = 8.dp, start = 16.dp, bottom = 8.dp),
+            color = Color.Black,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        checkedState.forEachIndexed { index, isChecked ->  //se maneja el estado de los checkbox de manera independiente
+
+            Row(
+                modifier = Modifier,
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.Start
+            )
+            {
+                Checkbox(
+                    checked = isChecked,
+                    onCheckedChange = {
+                        viewModel.SelectorEstado(index, it)
+                    }
+                )
+                Text(
+                    text = label[index],
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
+    }
+}
+@Composable
+fun CalendFechaDesde(viewModel: FacturaViewModel = viewModel()){
+    val selectedDate = viewModel.fechaDesde
     var showDate by remember { mutableStateOf(false) }
-    //var showDateHasta by remember { mutableStateOf(false) }
+
+    val formattedDate = selectedDate?.let {
+        val date = Date(it)
+        SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date)
+    } ?: stringResource(R.string.btn_fecha)
 
     Button(
         onClick = { showDate = true },
         modifier = Modifier,
-        colors = ButtonDefaults.buttonColors(Color.LightGray)
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.btn_fecha_color)),
+        shape = RoundedCornerShape(8.dp)
     ) {
         Text(
-            text = stringResource(R.string.btn_fecha),
+            text = formattedDate,
             color = Color.Black
         )
     }
-//    Button(onClick = {showDateHasta = true }) {
-//        Text(text = stringResource(R.string.btn_fecha))
-//    }
-
     if (showDate) {
         DatePickerModal(
-            onDateSelected = { selectedDate = it },
+            onDateSelected = {
+                viewModel.ActualizarFechaDesde(it)
+            },
             onDismiss = { showDate = false }
         )
     }
-//    if (showDateHasta){
-//        DatePickerModal(
-//            onDateSelected = { selectedDate = it},
-//            onDismiss = { showDateHasta = false}
-//        )
-//    }
+}
+
+@Composable
+fun CalendFechaHasta(
+    modifier: Modifier = Modifier,
+    viewModel: FacturaViewModel = viewModel()
+    ) {
+    val selectedDate = viewModel.fechaHasta
+    var showDate by remember { mutableStateOf(false) }
+
+    val formattedDate = selectedDate?.let {
+        val date = Date(it)
+        SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(date)
+    } ?: stringResource(R.string.btn_fecha)
+
+    Button(
+        onClick = { showDate = true },
+        modifier = Modifier,
+        colors = ButtonDefaults.buttonColors(colorResource(R.color.btn_fecha_color)),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Text(
+            text = formattedDate,
+            color = Color.Black
+        )
+    }
+    if (showDate) {
+        DatePickerModal(
+            onDateSelected = {
+                viewModel.ActualizarFechaHasta(it)
+            },
+            onDismiss = { showDate = false }
+        )
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -194,26 +313,41 @@ fun DatePickerModal(onDateSelected: (Long?) -> Unit, onDismiss: () -> Unit) {
     }
 }
 
-fun convertMillisToDate(millis: Long): String? {
-    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
-    return formatter.format(Date(millis))
-}
-
 @Composable
-fun Slider(importeMax: Float) {
-    var sliderPosition by remember { mutableStateOf(0f) }
-    Column(modifier = Modifier.padding(16.dp)) {
+fun SeleccionarImporte(importeMax: Float,
+                       viewModel: FacturaViewModel = viewModel()) {
+
+    val sliderPosition = viewModel.sliderPosition
+    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp))
+    {
 
         Text(
             text = "${sliderPosition.toInt()}€",
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally),
+            color = colorResource(R.color.screen_fact_color)
         )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.min_slider),
+                modifier = Modifier.padding(bottom = 4.dp, start = 8.dp),
+                color = Color.LightGray,
+            )
+            Text(
+                text = stringResource(R.string.max_slider),
+                modifier = Modifier.padding(bottom = 4.dp, start = 8.dp),
+                color = Color.LightGray,
+            )
+        }
         Slider(
             value = sliderPosition,
             onValueChange = { range ->
-                sliderPosition = range.toInt().toFloat() },
+                viewModel.SelectorImporte(range.toInt().toFloat())
+            },
             valueRange = 0f..importeMax,
-            //steps = (importeMax - 1).toInt(),
             colors = SliderDefaults.colors(
                 thumbColor = colorResource(R.color.screen_fact_color),   // Color de los "puntos" que se arrastran
                 activeTrackColor = colorResource(R.color.screen_fact_color),  // Color de la línea entre los thumbs
@@ -221,8 +355,6 @@ fun Slider(importeMax: Float) {
             ),
             modifier = Modifier.fillMaxWidth(),
         )
-
-
     }
 }
 
